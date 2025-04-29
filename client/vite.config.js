d76b6ed421798538be18ef3813e73150d7aaed88
vishnu <139614960+vishnuu5@users.noreply.github.com>
2025-04-29 11:00:07 +0530

    vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://galaxy-ai-sfgf.onrender.com",
         changeOrigin: true,
       },
     },
